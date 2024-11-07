eeff61444bf7558198f0af4233fcf66e546b34bd
Diogo Jorge <diogofernandoj@gmail.com>
2024-11-07 09:45:39 -0300

    feat: ensure transaction can be updated

--- app/transactions/_components/edit-transaction-button.tsx
+++ app/transactions/_components/edit-transaction-button.tsx
@@ -1,33 +1,35 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from "@/app/_components/ui/dialog";
+import UpsertTransactionForm from "@/app/_components/upsert-transaction-form";
 import { Transaction } from "@prisma/client";
 import { PencilIcon } from "lucide-react";
+import { useState } from "react";
 
 interface EditTransactionButtonProps {
   transaction: Transaction;
 }
 
 const EditTransactionButton = ({ transaction }: EditTransactionButtonProps) => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
   return (
-    <Dialog>
+    <>
       <Button
-        asChild
         variant="ghost"
         size="icon"
         className="text-muted-foreground"
+        onClick={() => setDialogIsOpen(true)}
       >
-        <DialogTrigger>
-          <PencilIcon />
-        </DialogTrigger>
+        <PencilIcon />
       </Button>
-      <DialogContent>{transaction.name}</DialogContent>
-    </Dialog>
+      <UpsertTransactionForm
+        isOpen={dialogIsOpen}
+        setIsOpen={setDialogIsOpen}
+        defaultValues={{ ...transaction, amount: Number(transaction.amount) }}
+        transactionId={transaction.id}
+      />
+    </>
   );
 };
 
