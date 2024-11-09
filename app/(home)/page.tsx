d7c49f5d9719c2d5ddaf18ed378bffa7c3b9e2b9
Diogo Jorge <diogofernandoj@gmail.com>
2024-11-09 10:09:39 -0300

    feat: add TransactionPieChart

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,9 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
-import { db } from "../_lib/prisma";
 import Header from "./_components/header";
 import { isMatch } from "date-fns";
+import { getDashboard } from "../_data/get-dashboard";
+import TransactionsPieChart from "./_components/transaction-pie-chart";
 
 const HomePage = async ({
   searchParams: { month },
@@ -20,53 +21,21 @@ const HomePage = async ({
     redirect(`/?month=${new Date().getMonth() + 1}`);
   }
 
-  const where = {
-    date: {
-      gte: new Date(`${new Date().getFullYear()}-${month}-01`),
-      lt: new Date(`${new Date().getFullYear()}-${month}-31`),
-    },
-  };
-
-  const depositsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { AND: [{ type: "DEPOSIT" }, where] },
-        _sum: { amount: true },
-      })
-    )._sum.amount,
-  );
-
-  const expensesTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { AND: [{ type: "EXPENSE" }, where] },
-        _sum: { amount: true },
-      })
-    )._sum.amount,
-  );
-
-  const investmentsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { AND: [{ type: "INVESTMENT" }, where] },
-        _sum: { amount: true },
-      })
-    )._sum?.amount,
-  );
-
-  const balance = depositsTotal - expensesTotal - investmentsTotal;
+  const dashboard = await getDashboard(month);
 
   return (
     <>
       <Navbar />
       <div className="space-y-6 p-6">
         <Header />
-        <SummaryCards
-          depositsTotal={depositsTotal}
-          expensesTotal={expensesTotal}
-          investmentsTotal={investmentsTotal}
-          balance={balance}
-        />
+        <div className="grid h-full grid-cols-[2fr,1fr] grid-rows-1 gap-6">
+          <div className="flex flex-col gap-6">
+            <SummaryCards {...dashboard} />
+            <div className="grid grid-cols-3">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
